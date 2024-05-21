947fa4ed74f9cda2681ae8facda75f4f83686bd5
Passionate Flutter developer <129858140+RomanDevelop@users.noreply.github.com>
2024-05-21 10:07:01 +0200

    a few correction done

--- lib/themes.dart
+++ lib/themes.dart
@@ -4,11 +4,11 @@ final ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
   primaryColor: Colors.blue,
   hintColor: Colors.blueAccent,
-  textTheme: TextTheme(
+  textTheme: const TextTheme(
     bodyLarge: TextStyle(color: Colors.black),
     bodyMedium: TextStyle(color: Colors.black54),
   ),
-  buttonTheme: ButtonThemeData(
+  buttonTheme: const ButtonThemeData(
     buttonColor: Colors.blue,
     textTheme: ButtonTextTheme.primary,
   ),
@@ -18,11 +18,11 @@ final ThemeData darkTheme = ThemeData(
   brightness: Brightness.dark,
   primaryColor: Colors.black,
   hintColor: Colors.redAccent,
-  textTheme: TextTheme(
+  textTheme: const TextTheme(
     bodyLarge: TextStyle(color: Colors.white),
     bodyMedium: TextStyle(color: Colors.white70),
   ),
-  buttonTheme: ButtonThemeData(
+  buttonTheme: const ButtonThemeData(
     buttonColor: Colors.red,
     textTheme: ButtonTextTheme.primary,
   ),
